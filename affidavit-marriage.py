4abf88a7891d03b6da7fd790ccc6568d6d3709df
emptyroom <shaunanderson5@gmail.com>
2017-12-07 13:55:36 -0500

    Updates to affidavit-marriage.py

--- affidavit-marriage.py
+++ affidavit-marriage.py
@@ -1,5 +1,6 @@
 from docx import Document
 from docx.shared import Pt
+from datetime import *
 
 document = Document('affidavit-template.docx')
 document.save('affidavit-new.docx')
@@ -14,12 +15,39 @@ spouseName = raw_input()
 print "What is the city and province?"
 location = raw_input()
 
+print "What date did the couple start living together?"
+livingsince = raw_input()
+
 title = document.add_heading('AFFIDAVIT OF MARRIAGE')
 title.style = document.styles['Heading 1']
 run = title.add_run()
 run.add_break()
 
-p = document.add_paragraph("We, %s and %s, both of %s, SOLEMNLY AFFIRM AND DECLARE THAT:" % (studentName, spouseName, location))
+p = document.add_paragraph(
+    "We, %s and %s, both of %s, SOLEMNLY AFFIRM AND DECLARE THAT:" % (
+    studentName, spouseName, location))
+
+run = p.add_run()
+run.add_break()
+
+p = document.add_paragraph(
+        "We are living together in a conjugal relationship, and have done so continuously since %s""" % livingsince)
+p.style = document.styles['ListNumber']
+run = p.add_run()
+run.add_break()
+
+p = document.add_paragraph(
+        "The information contained in this affidavit is provided for the sole purpose of supporting an application for financial aid under the Ontario Student Assistance Program (OSAP).")
+p.style = document.styles['ListNumber']
+run = p.add_run()
+run.add_break()
+
+p = document.add_paragraph(
+        "We understand that the acceptance of this affidavit by the Carleton University Awards Office and the Ontario Ministry of Advanced Education and Skills Development in no way constitutes a legal determination that our common law marriage is valid under applicable law.")
+p.style = document.styles['ListNumber']
+run = p.add_run()
+run.add_break()
+
 
 
 document.save('affidavit-new.docx')
