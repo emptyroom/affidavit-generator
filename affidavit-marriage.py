be535acbe51e6403aeda6deb4402564ec28c5425
Shaun Anderson <ShaunAnderson@cunet.carleton.ca>
2017-12-08 13:14:53 -0500

    merging with testing branch

--- affidavit-marriage.py
+++ affidavit-marriage.py
@@ -1,40 +1,37 @@
 from docx import Document
 from docx.shared import Pt
+import Tkinter
+import tkSimpleDialog
 import time
 import datetime
-
-i = time.strftime("%c")
-now = datetime.datetime.now()
 
 document = Document('affidavit-template.docx')
 document.save('affidavit-new.docx')
 document._body.clear_content()
 
+root = Tkinter.Tk()
+root.geometry("1000x300")
+root.withdraw()
+
+
+studentName = tkSimpleDialog.askstring("Student Name", "Please enter the student's name")
+spouseName =  tkSimpleDialog.askstring("Spouse Name", "Please enter the spouse's name")
+location = tkSimpleDialog.askstring("Location", "Please enter the city and province")
+livingsince = tkSimpleDialog.askstring("Date Living Since", "Please enter the date the couple began living together")
+
+i = time.strftime("%c")
+now = datetime.datetime.now()
+
 underline = "_______________________________________"
 
-for style in document.styles:
-    print style.name
-
-print "What is the student's first and last name?"
-studentName = raw_input()
-
-print "What is the spouse's first and last name?"
-spouseName = raw_input()
-
-print "What is the city and province?"
-location = raw_input()
-
-print "What date did the couple start living together?"
-livingsince = raw_input()
-
-title = document.add_heading('AFFIDAVIT OF MARRIAGE')
+title = document.add_heading('AFFIDAVIT OF MARITAL STATUS')
 title.style = document.styles['Heading 1']
 run = title.add_run()
 run.add_break()
 
 p = document.add_paragraph(
-    "We, %s and %s, both of %s, SOLEMNLY AFFIRM AND DECLARE THAT:" % (
-    studentName, spouseName, location))
+"We, %s and %s, both of %s, SOLEMNLY AFFIRM AND DECLARE THAT:" % (
+studentName, spouseName, location))
 
 run = p.add_run()
 run.add_break()
